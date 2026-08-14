e4f62d5d1f0fbc046ab10e0a6552a21642ae23fd
vahav11 <104652527+vahav11@users.noreply.github.com>
2026-08-14 00:02:26 -0700

    Add files via upload
    updated main.py that handles queries 1 2 and 4

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pages.signin import SigninPage
 from pages.welcome import WelcomePage
 from pages.browse import BrowsePage
 from pages.item_detail import ItemDetailPage
+from pages.item_manager import ItemManagerPage
 import sys
 import db
 import session
@@ -25,6 +26,7 @@ signin = SigninPage()
 welcome = WelcomePage()
 browse = BrowsePage()
 item_detail = ItemDetailPage()
+item_manager = ItemManagerPage()
 
 stack.addWidget(home)        # 0
 stack.addWidget(signup)      # 1
@@ -32,6 +34,7 @@ stack.addWidget(signin)      # 2
 stack.addWidget(welcome)     # 3
 stack.addWidget(browse)      # 4
 stack.addWidget(item_detail) # 5
+stack.addWidget(item_manager)# 6
 
 
 def do_signup():
@@ -39,6 +42,7 @@ def do_signup():
         signin.message.setStyleSheet("color: green;")
         signin.message.setText("Account created. Please sign in.")
         stack.setCurrentIndex(2)
+
 
 def do_signin():
     result = signin.attempt_signin()
@@ -48,19 +52,23 @@ def do_signin():
         welcome.greeting.setText(f"Welcome, {firstName} {lastName}")
         stack.setCurrentIndex(3)
 
-# navigation
+
+# --- Navigation Mapping ---
 home.signup_button.clicked.connect(lambda: stack.setCurrentIndex(1))
 home.signin_button.clicked.connect(lambda: stack.setCurrentIndex(2))
 signup.back_button.clicked.connect(lambda: stack.setCurrentIndex(0))
 signin.back_button.clicked.connect(lambda: stack.setCurrentIndex(0))
 signin.goto_signup_button.clicked.connect(lambda: stack.setCurrentIndex(1))
+
+
 def do_logout():
    session.current_user = None
    welcome.greeting.setText("")
    stack.setCurrentIndex(0)
 
 
-welcome.logout_button.clicked.connect(do_logout) #just in case
+welcome.logout_button.clicked.connect(do_logout)
+
 
 def open_browse():
     browse.load_categories()
@@ -72,13 +80,33 @@ def open_item(itemID):
     stack.setCurrentIndex(5)
 
 
-browse.on_open_item = open_item
+def open_item_manager_manage():
+    # Switches to the Item Manager page and selects the Manage tab (index 0)
+    item_manager.tabs.setCurrentIndex(0)
+    stack.setCurrentIndex(6)
 
+
+def open_item_manager_queries():
+    # Switches to the Item Manager page and selects the Queries tab (index 1)
+    item_manager.tabs.setCurrentIndex(1)
+    stack.setCurrentIndex(6)
+
+
+# Wire the Browse logic
+browse.on_open_item = open_item
 welcome.browse_button.clicked.connect(open_browse)
 browse.back_button.clicked.connect(lambda: stack.setCurrentIndex(3))
 item_detail.back_button.clicked.connect(open_browse)
 
-# actions
+# Wire the Item Manager / Queries logic based on welcome.py buttons
+welcome.post_item_button.clicked.connect(open_item_manager_manage)
+welcome.my_items_button.clicked.connect(open_item_manager_manage)
+welcome.queries_button.clicked.connect(open_item_manager_queries)
+
+# Wire the back button from item_manager to return to the Welcome tab
+item_manager.back_button.clicked.connect(lambda: stack.setCurrentIndex(3))
+
+# --- Actions ---
 signup.create_button.clicked.connect(do_signup)
 signin.signin_button.clicked.connect(do_signin)
 
